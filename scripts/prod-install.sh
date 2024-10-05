1648d1929903a9ec607aadeb587b12da0b05ead0
Sam Warnick <sam@warnick.me>
2024-10-05 13:31:56 -0400

    Add install

--- scripts/prod-install.sh
+++ scripts/prod-install.sh
@@ -6,6 +6,7 @@ BRANCH="main"
 BUN="$(which bun)"
 USER="$(whoami)"
 
+bun install
 WORKING_DIR=$WORKING_DIR PROJECT_NAME=$PROJECT_NAME BRANCH=$BRANCH envsubst < scripts/templates/post-receive > .git/hooks/post-receive
 chmod +x .git/hooks/post-receive
 mkdir tmp
